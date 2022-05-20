5dc4e58a178d061725462db14eda4615e5c44fc4
bridger-huhn <bhuhn@uwyo.edu>
2022-05-20 13:12:34 -0600

    improved the licor cleaner to accept many different licor formattes

--- LICOR_6400_Cleaner.R
+++ LICOR_6400_Cleaner.R
@@ -1,12 +1,18 @@
 library(tidyverse)
+### this program works best if your Remarks are in the formatte "number_typeofmeasurement" for example 6_light or 23_ACi
 
-#this sets the wd to whichever file you want to extract files from
-setwd("/Users/bridgerhuhn/Documents/Research/Blowout_Pen/DATA/2022GH/LICOR")
-#gets all files with the given file type
+### first step is to open your licor files in excel and save them all as .csv files
+
+#### then set your working directory to the folder with all the csv files you want to clean
+
+setwd("/Users/bridgerhuhn/Documents/Research/Blowout_Pen/DATA/2022GH/LICOR") ### change this line to be your file path
+
+#gets all files with the given file type should be .csv
 allFiles<-list.files(pattern = "*.csv")
 
 outDF<- data.frame() ### creates a dataframe to store
 
+## this for loop combines all the files into one big file
 for (i in 1:length(allFiles)){
   #for the current file read it in
   dat<- read.csv(allFiles[i], row.names = NULL)
@@ -26,12 +32,12 @@ for (i in 1:length(allFiles)){
   outDF<- rbind(outDF, dat)
 }
 dat <- outDF
-## this function gets wrid of rows that aren't necessary
+## this function gets rid of rows that aren't necessary 
 LC<-function(dat){
   #creates a comments column
   dat$comment<-"kitten"
-  
-  
+  dat <- dat[dat$HHMMSS != "in",] # deletes column names in the data frame
+  dat <- dat[dat$Obs != "Obs",]
   #This is to put comments in comment column####
   for (i in 1:nrow(dat)) {
     if (i>1){
@@ -43,7 +49,7 @@ LC<-function(dat){
     }
   }
   
-  #deletes rows where Remarks don't take measuremetns (Remarks that we didn't type in on the machine)
+  #deletes rows where Remarks don't take measurements (Remarks that we didn't type in on the machine)
   todelete<- c()
   for (i in 1:nrow(dat)) {
     if (i>1){
@@ -54,7 +60,7 @@ LC<-function(dat){
   }
   for (i in 1:nrow(dat)) {
     if (i>1){
-      if (grepl(pattern = "light|ACi|aci",dat[i,which(names(dat) == "comment")])) {
+      if (grepl(pattern = "_",dat[i,which(names(dat) == "comment")]) & dat[i,1] == "Remark=") {
         dat$comment[i] <- dat[i,2]
       } else {
         dat$comment[i]<- dat$comment[i-1]
@@ -64,29 +70,21 @@ LC<-function(dat){
   dat <- dat[-todelete,]
   
   ## deletes leading rows that where named "kitten" earlier ^^^^
-  dat <- dat[-which(dat$comment=="kitten"),] 
-  dat <- dat[which(grepl(pattern = ":", dat[,2])),]
-  #makes all numbers numeric and non numbers NAs
-  dat[,1]<-as.numeric(as.character(unlist(dat[,1])))
-  dat$comment <- sub("^.{1}", "", dat$comment)
-  dat$comment <- sub(".{2}$","", dat$comment)
-  dat$comment <- sub(".+ ","",dat$comment)
+  dat <- dat[-which(dat$comment=="kitten"),]
+  for (i in 1:nrow(dat)) {if(grepl(pattern = ":", dat$comment[i], fixed = TRUE)){ 
+    ## these lines us regular expressions to clean up the comments column
+    dat$comment[i] <- sub("^.{1}", "", dat$comment[i])
+    dat$comment[i] <- sub(".{2}$","", dat$comment[i])
+    dat$comment[i] <- sub(".+ ","",dat$comment[i])}}
+  dat <- dat[which(dat$Area != ""),] 
+  
+  #the following code turns all character columns into numeric
+
+  dat[cols.num[4:80]] <- sapply(sapply(dat[4:80],as.character),as.numeric)
   return(dat)
 }
 
 d<- LC(outDF)
 rm(outDF)
 
-d$comment[which(d$comment == "n")]<- "7_ACi"
-d<- d[-which(d$comment == "23_light"),]
-d$comment[which(d$comment =="23_light_redo/actual")]<- "23_light"
-
-require(tidyverse)
-ggplot(d %>% filter(grepl("ACi",comment)),aes(x = as.numeric(CO2R),y = as.numeric(PhiPS2), color = comment))+
-  geom_point()
-ggplot(d %>% filter(grepl("light",comment)),aes(x = as.numeric(PARi),y = as.numeric(PhiPS2), color = comment))+
-  geom_point()
-plot(d$PARi)
-ggplot(d %>% filter(as.numeric(d$PhiCO2)>0), aes(x = as.numeric(PhiCO2),y = as.numeric(PhiPS2)))+
-  geom_point()+
-  xlim(c(0,.03))
+### now the dataframe named "d" has a comment column that has all of your remarks in it
